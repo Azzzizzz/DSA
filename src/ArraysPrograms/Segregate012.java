438b364f88320b30b86e52271a9defa0898d35ec
Syed Abdul Aziz <syedaziz9999@gmail.com>
2023-10-02 14:57:16 +0530

    segregate with 3 pointers

--- src/ArraysPrograms/Segregate012.java
+++ src/ArraysPrograms/Segregate012.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 public class Segregate012 {
     public static void main(String[] args) {
         int[] arr = {0, 1, 2, 1, 1, 2, 0, 1, 2};
+//        int[] arr = {0, 1, 2, 2, 1, 0};
         System.out.println("Segregate 012");
-        segregate(arr);
+//        segregate(arr);
+        segregateWith3Pointers(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -32,5 +34,27 @@ public class Segregate012 {
         for (int i = count_0s + count_1s; i < n; i++) {
             arr[i] = 2;
         }
+    }
+
+    static void segregateWith3Pointers(int[] arr) {
+        int n = arr.length;
+        int p0 = 0, p1 = 0, p2 = n - 1;
+        while (p1 <= p2) {
+            if (arr[p1] == 0) {
+                swap(arr, p0, p1);
+                p0++;
+                p1++;
+            } else if (arr[p1] == 2) {
+                swap(arr, p2, p1);
+                p2--;
+            } else p1++;
+        }
+
+    }
+
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
