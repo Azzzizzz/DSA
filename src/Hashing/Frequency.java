83de93ad18c4bc6f95f51f6f5b65a3292eb27a96
Syed Abdul Aziz <syedaziz9999@gmail.com>
2023-10-17 15:30:26 +0530

    Frequency of characters with hashing

--- src/Hashing/Frequency.java
+++ src/Hashing/Frequency.java
@@ -1,12 +1,18 @@
 package Hashing;
 
+import java.util.Arrays;
+
 public class Frequency {
     public static void main(String[] args) {
 //        int[] arr = {1, 1, 0, 8, 8, 2, 8, 3, 2};
         int[] arr = {-1, -2, 2, 0, -1, 3};
+        String str = "ababacz";
         System.out.println("Print the frequency using hashing");
 //        frequency(arr);
-        frequencyWithNegatives(arr);
+//        frequencyWithNegatives(arr);
+     
+//        System.out.println(frequencyOfCharacters(str));
+        frequencyOfCharacters(str);
     }
 
     static void frequency(int[] arr) {
@@ -38,4 +44,26 @@ public class Frequency {
             }
         }
     }
+
+    static void frequencyOfCharacters(String str) {
+
+        int[] map = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+
+            int index = str.charAt(i) - 'a';
+            map[index] = map[index] + 1;
+
+
+        }
+        System.out.println(Arrays.toString(map));
+
+        for (int i = 0; i < 26; i++) {
+            int index = i + 'a';
+            if (map[i] != 0) {
+                System.out.println((char) index + ":" + map[i]);
+            }
+        }
+
+    }
+
 }
